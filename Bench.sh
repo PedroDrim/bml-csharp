50723d840667fa728dacd481b6a12303a9f72e16
pedro <pedrohenriquedrim@gmail.com>
2025-01-17 16:28:07 -0300

    inicio de testes

---
+++ Bench.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+dotnet run 100000
+dotnet run 200000
+dotnet run 300000
+dotnet run 400000
+dotnet run 500000
+dotnet run 600000
+dotnet run 700000
+dotnet run 800000
+dotnet run 900000
+dotnet run 1000000
