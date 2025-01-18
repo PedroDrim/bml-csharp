b3dc8b5bf47732f08db8717a62646be04e60fe57
pedro <pedrohenriquedrim@gmail.com>
2025-01-17 22:29:12 -0300

    casos de testes iniciados

--- simulation.tests/src/model/UserInfoTest.cs
+++ simulation.tests/src/model/UserInfoTest.cs
@@ -8,6 +8,6 @@ public class UserInfoTest {
     [Fact]
     public void MustBeInstantiable() {
         UserInfo userinfo = new UserInfo("user", "password");
-        
+        Assert.True(userinfo != null, "1. Devera ser instanciavel corretamente");
     }
 }
