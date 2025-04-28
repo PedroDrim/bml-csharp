fbe5a233e54ac18ce322a3b58744731395d08c76
pedro <pedrohenriquedrim@gmail.com>
2025-04-28 13:03:56 -0300

    corrigindo workflow

--- simulation.tests/obj/Debug/net9.0/simulation.tests.AssemblyInfo.cs
+++ simulation.tests/obj/Debug/net9.0/simulation.tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("simulation.tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b0b08e79f88d44f31e877d27a7d934ae49557a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5143bbf3f01db14ea16aac56a03ee90810af7383")]
 [assembly: System.Reflection.AssemblyProductAttribute("simulation.tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("simulation.tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
